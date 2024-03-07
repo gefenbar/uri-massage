e87a4f71ed961be3a218348914b34709763c2ac2
gefenbar <gefenbar23@gmail.com>
2024-03-07 14:08:18 +0200

    fixed redirect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar'; 
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import TreatmentsPage from './pages/Treatments';
-import Footer from './Components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import TreatmentsPage from "./pages/Treatments";
+import Footer from "./Components/Footer";
 import { AccessibilityWidget } from "react-accessibility";
-import './App.css'
-import SocialLinks from './Components/SocialLinks';
+import "./App.css";
+import SocialLinks from "./Components/SocialLinks";
 
 function App() {
-
   return (
     <div className="App">
-      <SocialLinks/>
-            <AccessibilityWidget />
+      <SocialLinks />
+      <AccessibilityWidget />
 
       <BrowserRouter>
         <Navbar />
 
         <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/treatments" element={<TreatmentsPage/>} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/treatments" element={<TreatmentsPage />} />
 
-        <Route path="/contact" element={<ContactPage />} />
-        
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-
     </div>
   );
-
 }
 
 export default App;
