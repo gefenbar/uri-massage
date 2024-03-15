c4ca2943f08ed08f663b5cf01a12e660958deb8a
gefenbar <gefenbar23@gmail.com>
2024-03-15 14:00:06 +0200

    new design updates

--- src/pages/Treatments.js
+++ src/pages/Treatments.js
@@ -1,28 +1,5 @@
 import { TreatmentsSection } from './HomePage';
 export default function TreatmentsPage() {
-  
-  const treatments = [
-    {
-      title: 'עיסוי שוודי',
-      image: 'swedish-massage.webp', 
-    },
-    {
-      title: 'עיסוי לנשים הרות',
-      image: 'pregnancy-massage.webp', 
-    },
-    {
-      title: 'עיסוי תינוקות',
-      image: 'baby-massage.webp', 
-    },
-    {
-      title: 'כוסות רוח',
-      image: 'cupping-glass-massage.webp', 
-    },
-    {
-      title: 'טיפ222ול בקריסטלים',
-      image: 'crystals-massage.webp', 
-    }
-  ];
     return (  
         <main className="treatment-page">
 
