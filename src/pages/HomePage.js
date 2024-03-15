b3881e3140163c3bd62d0b0f2a11fe370dbf2d26
gefenbar <gefenbar23@gmail.com>
2024-03-15 15:45:41 +0200

    new design updates

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,74 +1,87 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    return (
-      <main className="home">
-  <HeroSection/>
-  <AboutSection/>
-  <TreatmentsSection/>
-  <ContactSection/>
-      </main>
-    );
-  }
-  
-  const HeroSection = ()=>(
+  return (
+    <main className="home">
+      <HeroSection />
+      <AboutSection />
+      <TreatmentsSection />
+      <ContactSection />
+    </main>
+  );
+}
+
+const HeroSection = () => (
   <section className="hero-section">
-  <div className="hero-content">
-  <h1>אורי שמאי מטפל במגע וצליל</h1>
-  <p>מגוון טיפולים מרגיעים ומאזנים</p>
-<br/>
-  <button><Link to="/treatments">לתיאום טיפול</Link>
-  </button>  </div>
+    <div className="hero-content">
+      <h1>אורי שמאי מטפל במגע וצליל</h1>
+      <p>מגוון טיפולים מרגיעים ומאזנים</p>
+      <br />
+      <button>
+        <Link to="/contact">לתיאום טיפול</Link>
+      </button>
+    </div>
   </section>
-  )
-  const AboutSection = () => (
-<section className="about-section">
-  <div className="about-container">
-    <div className="about-content">
-      <div className="about-image-container">
-        <img src="uri.webp" alt="אורי שמאי" width="400px" height="500px" />
-      </div>
-      <div className="about-text-container">
-        <h2>קצת עליי</h2>
-        <p>
-          אורי שמאי - מטפל מוסמך, בוגר המכללה לרפואה אלטרנטיבית תאילנד קוסמוי ומכללת ברק. 
-          אני מבצע מגוון טיפולים, ביניהם: טיפול בצלילים מרפאים (Sound Healing),  עיסוי שוודי, עיסוי לנשים בהריון, עיסוי תינוקות, טיפול בקריסטלים ועוד.
-        </p>
-        <Link to="/contact" >לפרטים נוספים וקביעת תור</Link>
+);
+const AboutSection = () => (
+  <section className="about-section">
+    <div className="about-container">
+      <div className="about-content">
+        <div className="about-image-container">
+          <img src="uri.webp" alt="אורי שמאי" width="400px" height="500px" />
+        </div>
+        <div className="about-text-container">
+          <h2>קצת עליי</h2>
+          <p>
+            אורי שמאי - מטפל מוסמך, בוגר המכללה לרפואה אלטרנטיבית תאילנד קוסמוי
+            ומכללת ברק.  אני מבצע מגוון טיפולים, ביניהם: טיפול בצלילים מרפאים
+            (Sound Healing), עיסוי שוודי, עיסוי לנשים בהריון, עיסוי תינוקות,
+            טיפול בקריסטלים ועוד.
+          </p>
+          <Link to="/contact">לפרטים נוספים וקביעת תור</Link>
+        </div>
       </div>
     </div>
-  </div>
-</section>
-
+  </section>
 );
 
-  
-  const treatments = [
+export const TreatmentsSection = ({
+  additionalTreatments,
+  showLink = true, showTitle=true
+}) => {
+  let treatments = [
     {
-      title: 'עיסוי שוודי',
-      image: 'swedish-massage.webp', 
+      title: "עיסוי תינוקות",
+      image: "baby-massage.webp",
     },
     {
-      title: 'עיסוי לנשים הרות',
-      image: 'pregnancy-massage.webp', 
+      title: "Sound Healing",
+      image: "soundHealing.webp",
     },
     {
-      title: 'עיסוי תינוקות',
-      image: 'baby-massage.webp', 
+      title: "עיסוי לנשים הרות",
+      image: "pregnancy-massage.webp",
     },
     {
-      title: 'כוסות רוח',
-      image: 'cupping-glass-massage.webp', 
+      title: "כוסות רוח",
+      image: "cups.webp",
     },
     {
-      title: 'טיפול בקריסטלים',
-      image: 'crystals-massage.webp', 
-    }
+      title: "טיפול בקריסטלים",
+      image: "crystals-massage.webp",
+    },
   ];
-  export const TreatmentsSection = () => (
+
+  if (Array.isArray(additionalTreatments)) {
+    treatments = [...treatments, ...additionalTreatments];
+  }
+
+  return (
     <section className="treatments-section">
+      {showTitle &&(
       <h2>טיפולים</h2>
+      )}
       <div className="treatment-cards">
         {treatments.map((treatment, index) => (
           <div className="treatment-card" key={index}>
@@ -80,24 +93,27 @@ export default function HomePage() {
             <p className="treatment-title">{treatment.title}</p>
           </div>
         ))}
-        
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <Link className="more" to="/treatments">לטיפולים נוספים</Link>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
+      {showLink && (
+        <Link className="more" to="/treatments">
+          לטיפולים נוספים
+        </Link>
+      )}
+      <br />
+      <br />
     </section>
-  )
-  const ContactSection = () => ( 
+  );
+};
+
+const ContactSection = () => (
   <section className="contact-section">
-  <h2>יצירת קשר</h2>
-  <p>כתובת: קיבוץ ארז</p> 
-  <p>טלפון: 0507431189</p>
-  <p>אימייל: uriss100@gmail.com</p>
-  <p>שעות פתיחה: א'-ה' 9:00-17:00, ו' בתאום מראש</p>
+    <h2>יצירת קשר</h2>
+    <p>כתובת: קיבוץ ארז</p>
+    <p>טלפון: 0507431189</p>
+    <p>אימייל: uriss100@gmail.com</p>
+    <p>שעות פתיחה: א'-ה' 9:00-17:00, ו' בתאום מראש</p>
   </section>
-  )
-
-
+);
--- src/pages/Treatments.js
+++ src/pages/Treatments.js
@@ -1,9 +1,22 @@
-import { TreatmentsSection } from './HomePage';
+import { TreatmentsSection } from "./HomePage";
 export default function TreatmentsPage() {
-    return (  
-        <main className="treatment-page">
+  const moreTreatments = [
+    { title: "עיסוי מקלות במבוק", image: "swedish-massage.webp" },
+    { title: " עיסוי שוודי", image: "swedish-massage.webp" },
+    { title: "פוט מסאז' תאילנדי", image: "swedish-massage.webp" },
+    { title: "נרות הופי", image: "swedish-massage.webp" },
+    { title: "רפלקסולוגיה", image: "swedish-massage.webp" },
+    { title: "עיסוי פנים", image: "swedish-massage.webp" },
+  ];
+  return (
+    <main className="treatment-page">
+      <h1>הטיפולים שלי</h1>
 
-<TreatmentsSection/>
-</main>
-    );
-  }
+      <TreatmentsSection
+        additionalTreatments={moreTreatments}
+        showLink={false}
+        showTitle={false}
+      />
+    </main>
+  );
+}
